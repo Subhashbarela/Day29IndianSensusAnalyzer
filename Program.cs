d60691876b61bdca45061a8fed23b9a07fbd77c7
Subhash Barela <barelasubhash16@gmail.com>
2023-03-15 17:41:49 +0530

    added State Sensus data

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Day29StateSensusProgram.StateSensus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day29StateSensusProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\Shiva027\\Desktop\\BridgeLabSolution\\Day29StateSensusProgram\\Day29StateSensusProgram\\StateSensus\\Book1.csv";
+            StateCensusAnalyser analyser = new StateCensusAnalyser(filePath);
+            int numberOfRecords = analyser.LoadDataFromCSV();
+            Console.WriteLine("Number of records in the file: " + numberOfRecords);
+
+        }
+    }
+}
---
+++ StateCensusAnalyser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day29StateSensusProgram.StateSensus
+{
+    public class StateCensusAnalyser
+    {
+        private string filePath;
+
+        public StateCensusAnalyser(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+
+        public int LoadDataFromCSV()
+        {
+            int numberOfRecords = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] fields = line.Split(',');
+                      
+                        numberOfRecords++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return numberOfRecords;
+        }
+    }
+}
